92e1b2640a352338eb54a57f24bbab913fd15f3f
Miro Hrabcak <hrabcak@procaut.onmicrosoft.com>
2022-02-18 21:23:23 +0100

    NB Dell commit 01

---
+++ Test1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test1
+{
+    //MH: vytvorene 18.02.2022 na NB Dell 21:15
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
